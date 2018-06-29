763b18c5a9df11d02c1f7e29faa4636777bf8176
unknown <5161005@sophia.local>
2018-06-29 13:58:04 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginInfoComponent } from './login-info/login-info.component';
+import { ChapterComponent } from './chapter/chapter.component';
+import { ChapterListComponent } from './chapter-list/chapter-list.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DetailComponent } from './detail/detail.component';
+import { EditaComponent } from './edita/edita.component';
+import { DefaultTipComponent } from './default-tip/default-tip.component';
+import { UserTipComponent } from './user-tip/user-tip.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginInfoComponent,
+    ChapterComponent,
+    ChapterListComponent,
+    DetailComponent,
+    EditaComponent,
+    DefaultTipComponent,
+    UserTipComponent,
+    OutputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-tip/user-tip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-tip',
+  templateUrl: './user-tip.component.html',
+  styleUrls: ['./user-tip.component.css']
+})
+export class UserTipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
