4b70c855c2b6987b46e1c43cde334563e6b906b7
unknown <5161003@sophia.local>
2018-07-23 09:25:23 +0900

    core整形

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserTipComponent } from './user-tip/user-tip.component';
 import { OutputComponent } from './output/output.component';
-import {CoreComponent} from './core/core.component';
+import { CoreComponent } from './core/core.component';
 import { LoginInfoComponent } from './login-info/login-info.component';
 import { HeaderComponent } from './header/header.component';
 import { EditaComponent } from './edita/edita.component';
@@ -13,9 +13,10 @@ import { DefaultTipComponent } from './default-tip/default-tip.component';
 import { ChapterListComponent } from './chapter-list/chapter-list.component';
 import { ChapterComponent } from './chapter/chapter.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { MatButtonModule} from '@angular/material';
-import {MatFormFieldModule} from '@angular/material';
-import {  MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatDividerModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -36,11 +37,12 @@ import {  MatInputModule } from '@angular/material';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule 
+    MatInputModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
