e0cd6b58127e8b99ae66e6c03186cb9361068ce6
Kazi0321 <5161014@sophia.local>
2018-09-11 10:20:13 +0900

    修正:不要な文削除

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chapter.component.css']
 })
 export class ChapterComponent implements OnInit {
-  test="<h1>hello</h1>"
+
   constructor() { }
 
   ngOnInit() {
